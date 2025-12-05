46fb72767cffb0e38d8d5ae6c9128b5a5179a112
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-12-05 14:09:50 -0600

    change brackets, and semi colons to commas

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ class Person{
     this.lastName= lastName;
   }
 }
-const guests = {
-  new Person("Bob", "Jones");
-  new Person("John", "Smith");
-  new Person("Tom", "Banks");
-  };
+const guests = [
+  new Person("Bob", "Jones"),
+  new Person("John", "Smith"),
+  new Person("Tom", "Banks"),
+  ];
 
 console.log(guests);
 
 //should define a class named Person that takes two parameters: firstName and lastName when it's created
 //should create a list named guests that contains three instances of the Person class
+
 
