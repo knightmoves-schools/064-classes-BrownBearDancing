f4901c4ac89ebfa9b2e5bc725e923ff46d96a7a6
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-12-05 13:55:30 -0600

    check first bullet

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+class Person{
+  constructor(firstName,lastName) {
+    this.firstName = firstName;
+    this.lastName= lastName;
+  }
+}
 
+should define a class named Person that takes two parameters: firstName and lastName when it's created
+should create a list named guests that contains three instances of the Person class
