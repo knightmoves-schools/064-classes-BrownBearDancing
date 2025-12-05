5ab2dbba6e47006100e94d1626a26cbaf25c21d3
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-12-05 14:08:14 -0600

    check if this works

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ class Person{
     this.lastName= lastName;
   }
 }
+const guests = {
+  new Person("Bob", "Jones");
+  new Person("John", "Smith");
+  new Person("Tom", "Banks");
+  };
 
-should define a class named Person that takes two parameters: firstName and lastName when it's created
-should create a list named guests that contains three instances of the Person class
+console.log(guests);
+
+//should define a class named Person that takes two parameters: firstName and lastName when it's created
+//should create a list named guests that contains three instances of the Person class
+
